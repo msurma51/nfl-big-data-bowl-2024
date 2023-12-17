047d69a4fb1ec6574a11d5c163f0a4b1bf816073
Mark Surma <surmam511@gmail.com>
2023-12-17 09:43:42 -0500

    initial src add

---
+++ src/definitions/name_space.py
@@ -0,0 +1,7 @@
+ol_group = ['C', 'G', 'T']
+off_skill_group = ['RB', 'FB', 'TE', 'WR'] 
+dl_group = ['DT', 'DE']
+lb_group = ['OLB', 'ILB']
+db_group = ['DB', 'SS', 'FS', 'CB']
+off_group = ['QB'] + off_skill_group + ol_group
+def_group = dl_group + lb_group + db_group
